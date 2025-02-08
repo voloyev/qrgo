d81e250e92406d917f8b65eb32542d81aaee7b1c
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2025-02-08 16:08:45 +0200

    Create basic cli

---
+++ go.mod
@@ -0,0 +1,17 @@
+module voloyev/qrgo
+
+go 1.23.5
+
+require (
+	github.com/urfave/cli/v3 v3.0.0-beta1
+	github.com/yeqown/go-qrcode/v2 v2.2.4
+	github.com/yeqown/go-qrcode/writer/standard v1.2.4
+)
+
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/yeqown/reedsolomon v1.0.0 // indirect
+	golang.org/x/image v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
+)
+
+func generateQrCodeActio(ctx context.Context, cmd *cli.Command) error {
+	text := cmd.Args().Get(0)
+	if text == "" {
+		return errors.New("text is required")
+	}
+
+	qrc, err := qrcode.New(text)
+	if err != nil {
+		return errors.New(fmt.Sprintf("could not generate QRCode: %v", err))
+	}
+
+	w, err := standard.New("repo-qrcode.jpeg")
+	if err != nil {
+		return errors.New(fmt.Sprintf("standard.New failed: %v", err))
+	}
+
+	if err = qrc.Save(w); err != nil {
+		return errors.New(fmt.Sprintf("could not save image: %v", err))
+	}
+
+	return nil
+}
+
+func main() {
+	cmd := &cli.Command{
+		Name:   "Generate QR code",
+		Usage:  "fight the loneliness!",
+		Action: generateQrCodeActio,
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
